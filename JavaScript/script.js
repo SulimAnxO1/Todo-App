d971f26861d231814a4861eb105395ca7cf6dff1
SulimAnxO1 <mxsulaimansufian@gmail.com>
2025-10-18 14:22:29 +0600

    feat: implement js logic

---
+++ JavaScript/script.js
@@ -0,0 +1,16 @@
+// DOM Elements
+const taskInput = document.getElementById("task-input");
+const addTaskBtn = document.getElementById("add-task");
+const todosList = document.getElementById("todos-list");
+const itemsLeft = document.getElementById("items-left");
+const clearCompletedBtn = document.getElementById("clear-completed");
+const emptyState = document.querySelector(".empty-state");
+const dateElement = document.getElementById("date");
+const filters = document.querySelectorAll(".filter");
+
+let todos = [];
+let currentFilter = "all";
+
+addTaskBtn.addEventListener("click", () => {
+  addTodo(taskInput.value);
+});
